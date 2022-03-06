c4b380c400ea59f4c71c6d3ca5890c6bff531fda
Rob Salamat <95326926+robsalamat@users.noreply.github.com>
2022-03-06 11:34:01 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,24 +1,36 @@
-import datetime as dt
-import numpy as np
-import pandas as pd
+
+import datetime as dt  
+import pandas as pd  
+import numpy as np  
+# sqlalchemy to connect with DB
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+
+# flask module
 from flask import Flask, jsonify
 
+
+# set up the DB
 engine = create_engine("sqlite:///hawaii.sqlite")
+
 Base = automap_base()
-Base.prepare(engine, reflect=True)
-Measurement = Base.classes.measurement
-Station = Base.classes.station
+Base.prepare(engine, reflect= True)
 
-session = Session(engine)
+measurement = Base.classes.measurement
+station = Base.classes.station
 
+session =  Session(engine)
+# set up flask app
 app = Flask(__name__)
-@app.route("/")
+
+
+
+    
+@app.route('/')
 def welcome():
-    return(
+    return (
     '''
     Welcome to the Climate Analysis API!
     Available Routes:
@@ -27,47 +39,54 @@ def welcome():
     /api/v1.0/tobs
     /api/v1.0/temp/start/end
     ''')
-@app.route("/api/v1.0/precipitation")
 
+@app.route('/api/v1.0/precipitation')
 def precipitation():
-   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
-   precipitation = session.query(Measurement.date, Measurement.prcp).\
-    filter(Measurement.date >= prev_year).all()
-   precip = {date: prcp for date, prcp in precipitation}
-   return jsonify(precip)
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days= 365)
+    precipitation = session.query(measurement.date, measurement.prcp).\
+        filter(measurement.date>= prev_year).all()
+    precip_js = {date: prcp for date, prcp in precipitation}
 
-@app.route("/api/v1.0/stations")
+    return jsonify(precip_js)
 
+@app.route('/api/v1.0/stations')
 def stations():
-    results = session.query(Station.station).all()
+    results = session.query(station.station).all()
     stations = list(np.ravel(results))
-    return jsonify(stations=stations)
 
-@app.route("/api/v1.0/tobs")
+    return jsonify(stations)
 
+@app.route('/api/v1.0/tobs')
 def temp_monthly():
-    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days= 365)
+    results = session.query(measurement.tobs).\
+        filter(measurement.station == 'USC00519281').\
+        filter(measurement.date >= prev_year).all()
+    temp = list(np.ravel(results))
 
-    results = session.query(Measurement.tobs).\
-    filter(Measurement.station == 'USC00519281').\
-    filter(Measurement.date >= prev_year).all()
-    temps = list(np.ravel(results))
-    return jsonify(temps=temps)
-
+    return jsonify(temp)
 @app.route("/api/v1.0/temp/<start>")
-@app.route("/api/v1.0/temp/<end>")
-
+@app.route("/api/v1.0/temp/<start>/<end>")
 def stats(start=None, end=None):
-        sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]           
-
-    if not end: 
-       results = session.query(*sel).\
-        filter(Measurement.date <= start).all()
-            temps = list(np.ravel(results))
-            return jsonify(temps)
-
-    results = session.query(*sel).\
-           filter(Measurement.date >= start).\
-         filter(Measurement.date <= end).all()
+    sel = [func.min(measurement.tobs), func.avg(measurement.tobs),\
+        func.max(measurement.tobs)]
+    
+    if not end:
+        results = session.query(*sel).filter(measurement.date>=start).\
+        filter(measurement.date<=end).all()
+    
+        temps = list(np.ravel(results))
+        return jsonify(temps)
+    
+    results = session.query(*sel).filter(measurement.date>=start).\
+    filter(measurement.date<=end).all()
     temps = list(np.ravel(results))
-    return jsonify(temps=temps)
+    return jsonify(temps)
+
+# check which destination execute app.py 
+print("example __name__ = %s", __name__)
+if __name__ == "__main__":
+    print("example is being run directly.")
+    app.run()
+else:
+    print("example is being imported")
